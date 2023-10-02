7fe1df69b97595d5e210bc355468188301424b63
Artur Jaworski <artur.jaworski@htdevelopers.com>
2023-10-02 22:08:41 +0200

    fix hygraph revalidating

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,17 +1,36 @@
+import { verifyWebhookSignature } from "@hygraph/utils";
 import { revalidatePath } from "next/cache";
 import { type NextRequest } from "next/server";
 
 export async function POST(request: NextRequest): Promise<Response> {
 	const body: unknown = await request.json();
 
+	const signature = request.headers.get("gcms-signature");
+	const secret = process.env.GRAPHQL_WEBHOOK_SECRET;
+
+	if (!signature || !secret) {
+		return new Response(null, { status: 400 });
+	}
+
+	const isValid = verifyWebhookSignature({ body: body, signature, secret });
+
+	if (!isValid) {
+		return new Response(null, { status: 400 });
+	}
+
 	if (
 		typeof body === "object" &&
 		body &&
-		"productId" in body &&
-		typeof body.productId === "string"
+		"data" in body &&
+		typeof body.data === "object" &&
+		body.data &&
+		"id" in body.data &&
+		"__typename" in body.data &&
+		typeof body.data.id === "string" &&
+		body.data.__typename === "Product"
 	) {
-		console.log(`Revalidating /product/${body.productId}`);
-		revalidatePath(`/product/${body.productId}`);
+		console.log(`Revalidating /product/${body.data.id}`);
+		revalidatePath(`/product/${body.data.id}`);
 		console.log(`Revalidating /products`);
 		revalidatePath(`/products`);
 		return new Response(null, { status: 204 });
